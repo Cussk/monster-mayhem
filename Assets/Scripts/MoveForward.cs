509640ca7fd2d9646bd6399cfb57d621a0e3519c
Kyle Cuss <cussprogramming@gmail.com>
2022-12-13 08:16:08 -0330

    added powerup pickup and action assets

--- Assets/Scripts/MoveForward.cs
+++ Assets/Scripts/MoveForward.cs
@@ -11,7 +11,7 @@ public class MoveForward : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        transform.Translate(Vector3.left * Time.deltaTime * speed);
         Destroy(gameObject, aliveTimer);
     }
 
