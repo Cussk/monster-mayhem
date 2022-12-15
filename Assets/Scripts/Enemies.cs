dcf9c01c381904b8bfd003d83938d88f02fc6417
Kyle Cuss <cussprogramming@gmail.com>
2022-12-15 19:05:29 -0330

    changed update method in enemies and PC scripts

--- Assets/Scripts/Enemies.cs
+++ Assets/Scripts/Enemies.cs
@@ -34,7 +34,7 @@ public class Enemies : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         if (gameManager.isGameActive)
         {
