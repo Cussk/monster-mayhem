f3957a69aa8bb2a78b08e9b5a2878eb9a1d42075
Kyle Cuss <cussprogramming@gmail.com>
2022-12-15 19:51:38 -0330

    fixed movement issues

--- Assets/Scripts/Enemies.cs
+++ Assets/Scripts/Enemies.cs
@@ -34,7 +34,7 @@ public class Enemies : MonoBehaviour
     }
 
     // Update is called once per frame
-    void FixedUpdate()
+    void Update()
     {
         if (gameManager.isGameActive)
         {
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -30,7 +30,7 @@ public class PlayerController : MonoBehaviour
     }
 
     // Update is called once per frame
-    void FixedUpdate()
+    void Update()
     {
         if (gameManager.isGameActive)
         {
